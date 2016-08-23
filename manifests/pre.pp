c797d6201c61475ee4963937b88635b79ffb8b00
Sean Brisbane <s.brisbane1@physics.ox.ac.uk>
2016-08-23 17:12:33 +0100

    Renumber rules more sensibly

--- manifests/pre.pp
+++ manifests/pre.pp
@@ -22,56 +22,36 @@ class oxphys_fw::pre {
   firewallchain { 'puppet:filter:IPv4':
     ensure  => present,
   }->
-  firewall { '001 puppet firewall CHAIN':
+  firewall { '002 puppet firewall CHAIN':
     chain   => 'INPUT',
     jump    => 'puppet',
   }->
 
-  firewall { '001 accept all icmp':
+  firewall { '010 accept all icmp':
     proto   => 'icmp',
     action  => 'accept',
     chain => puppet,
   }->
-  firewall { '002 accept all to lo interface':
+  firewall { '011 accept all to lo interface':
     proto   => 'all',
     iniface => 'lo',
     action  => 'accept',
     chain => puppet,
   }->
-  firewall { '003 accept related established rules':
+  firewall { '012 accept related established rules':
     proto   => 'all',
     state   => ['RELATED', 'ESTABLISHED'],
     action  => 'accept',
     chain => puppet,
   }
 
- firewall { "004 nagios access": 
+ firewall { "013 nagios access": 
    proto => "tcp", 
    dport=> "5666",
    action => accept,
    source => '163.1.136.128/32',
    chain => puppet,
  }
-#  firewall { "003 management access":
-#  proto  => "tcp",
-#  dport => [ 1839, 834, 835] ,
-#  action => "accept",
-#  }
-#  firewall { "004 file system access tcp":
-#  proto  => "tcp",
-#  dport => [ 2049, 111, 875, 32803, 892, 662] ,
-#  action => "accept",
-#  }->
-#  firewall { "005 file system access udp":
-#  proto  => "udp",
-#  dport => [ 32769 ,] ,
-#  action => "accept",
-#  }->
-#  firewall {"006 ypbind":
-#  proto  => ["tcp", "udp"],
-#  dport => [ 883] ,
-#  action => "accept",
-#  }
 
 } 
 
